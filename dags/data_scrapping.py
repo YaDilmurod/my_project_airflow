422d44acec13dab5140312cb83869b79ae3319ff
00010983 <00010983wiut@gmail.com>
2024-05-13 13:29:36 +0500

    Init

--- dags/data_scrapping.py
+++ dags/data_scrapping.py
@@ -1,34 +1,27 @@
 from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
-from airflow.utils.dates import days_ago
 import subprocess
 
-def execute_notebook():
-    subprocess.run(["/usr/local/bin/python3", "/Users/didi/Desktop/data_scrapping/Data Scrapping/Scrapping/02.Dom_uz.py"])
-
-def execute_notebook():
-    subprocess.run(["/usr/local/bin/python3", "/Users/didi/Desktop/data_scrapping/Data Scrapping/Scrapping/03.Joymee.py"])
-    
-def execute_notebook():
-    subprocess.run(["/usr/local/bin/python3", "/Users/didi/Desktop/data_scrapping/Data Scrapping/Scrapping/04.Local.py"])
-
-def execute_notebook():
-    subprocess.run(["/usr/local/bin/python3", "/Users/didi/Desktop/data_scrapping/Data Scrapping/Scrapping/05.Olx.py"])
-
-def execute_notebook():
-    subprocess.run(["/usr/local/bin/python3", "/Users/didi/Desktop/data_scrapping/Data Scrapping/Scrapping/06.Uybor.py"])
+def execute_notebooks():
+    scripts = [
+        "/Users/didi/Desktop/data_scrapping/Data Scrapping/Scrapping/02.Dom_uz.py",
+        "/Users/didi/Desktop/data_scrapping/Data Scrapping/Scrapping/03.Joymee.py",
+        "/Users/didi/Desktop/data_scrapping/Data Scrapping/Scrapping/04.Local.py",
+        "/Users/didi/Desktop/data_scrapping/Data Scrapping/Scrapping/05.Olx.py",
+        "/Users/didi/Desktop/data_scrapping/Data Scrapping/Scrapping/06.Uybor.py"
+    ]
+    for script in scripts:
+        subprocess.run(["/usr/local/bin/python3", script])
 
 dag = DAG(
-    'scrapping_dom_uz',
-    default_args={'start_date': days_ago(1)},
-    schedule_interval='0 23 * * *',
+    'scrapping',
     catchup=False
 )
 
-execute_notebook_task = PythonOperator(
-    task_id='execute_notebook',
-    python_callable=execute_notebook,
+execute_notebooks_task = PythonOperator(
+    task_id='execute_scrapping',
+    python_callable=execute_notebooks,
     dag=dag
 )
 
-execute_notebook_task
+execute_notebooks_task
